7a90dd7be9a8c94cdd7f0ddc7551ecd6f4b0c547
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-08-14 14:13:18 +0200

    Add console-kernel

---
+++ src/Console/Kernel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Latus\Laravel\Console;
+
+
+class Kernel extends \Illuminate\Foundation\Console\Kernel
+{
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
