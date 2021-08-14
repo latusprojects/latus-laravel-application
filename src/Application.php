c825f8d7c074c1f00ce3919b116feb438bbb2992
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-08-14 14:19:18 +0200

    Add Application.php and Bootstrapper.php

---
+++ src/Application.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Latus\Laravel;
+
+use Illuminate\Foundation\Application as LaravelApplication;
+
+class Application extends LaravelApplication
+{
+
+    protected array $additionalBaseProviders = [];
+    protected string|null $publicPath = null;
+
+    public function __construct($basePath = null, array $additionalBaseProviders = [])
+    {
+        parent::__construct($basePath);
+
+        $this->additionalBaseProviders = $additionalBaseProviders;
+    }
+
+    protected function registerBaseServiceProviders()
+    {
+        parent::registerBaseServiceProviders();
+
+        if (!empty($this->additionalBaseProviders)) {
+            foreach ($this->additionalBaseProviders as $baseProvider) {
+                $this->register($baseProvider);
+            }
+        }
+    }
+
+    public function usePublicPath(string|null $path)
+    {
+        $this->publicPath = $path;
+    }
+
+    public function publicPath(): string
+    {
+        return $this->publicPath ?? parent::publicPath();
+    }
+}
---
+++ src/Bootstrapper.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Latus\Laravel;
+
+use Latus\Laravel\Exceptions\Handler;
+use Latus\Laravel\Http\Kernel as HttpKernel;
+use Latus\Laravel\Console\Kernel as ConsoleKernel;
+
+
+class Bootstrapper
+{
+
+    protected Application|null $app = null;
+    protected array $baseProviders = [];
+    protected string $basePath = '';
+
+    public function __construct(string $basePath)
+    {
+        $this->basePath = $basePath;
+    }
+
+    public function addBaseProviders(array|string $baseProviders)
+    {
+        $this->baseProviders += $baseProviders;
+    }
+
+    public function build()
+    {
+        $this->app = new Application($this->basePath, $this->baseProviders);
+        $this->createBaseBindings();
+    }
+
+    protected function createBaseBindings()
+    {
+        $this->app->singleton(
+            \Illuminate\Contracts\Http\Kernel::class,
+            HttpKernel::class
+        );
+
+        $this->app->singleton(
+            \Illuminate\Contracts\Console\Kernel::class,
+            ConsoleKernel::class
+        );
+
+        $this->app->singleton(
+            \Illuminate\Contracts\Debug\ExceptionHandler::class,
+            Handler::class
+        );
+    }
+
+    public function finish()
+    {
+        $this->app->boot();
+    }
+
+    public function getApp(): Application|null
+    {
+        return $this->app;
+    }
+}
