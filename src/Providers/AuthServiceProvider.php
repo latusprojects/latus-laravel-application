ea11a6bf269b9ac44ddc42c3ae364fab6408db11
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-08-31 20:45:10 +0200

    Fix user-provider not being registered

--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -2,8 +2,24 @@
 
 namespace Latus\Laravel\Providers;
 
+use Illuminate\Contracts\Auth\UserProvider;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Auth::provider('service', function ($app, array $config) {
+
+            return $app->make(\Latus\Permissions\Services\UserProvider::class);
+        });
+    }
 }
